4bb290510567861b1f5f4622e5d4e9fccbbe125c
JOOYUNHAK <wndbsgkr@naver.com>
2025-03-06 00:23:34 +0900

    add: 축약 금지

---
+++ step5/index.ts
@@ -0,0 +1,16 @@
+// 5. 축약 금지
+// export class Coupon {
+//   private ctc: string;
+
+//   getCtc(): string {
+//     return this.ctc;
+//   }
+// }
+
+export class Coupon {
+  private couponTemplateCode: string;
+
+  getCouponTemplateCode(): string {
+    return this.couponTemplateCode;
+  }
+}
